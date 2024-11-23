80439797c857f2619cb0e820c8e1aa0b1d670233
azkaa2011 <azka050511@gmail.com>
2024-11-23 10:50:12 +0700

    Create html.js

---
+++ html.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const htmlContent = `
+       <!-- Ganti Audio di sini -->
+   <audio src="https://inspirasi.vercel.app/melody2.mp3" id="linkmp3" class="sembunyi"></audio>
+   
+   <div id="bodyblur">
+     <!-- Wallpaper --><img src="https://feeldreams.github.io/bromo.jpg" id="wallpaper"/><div id="beneranblur"></div>
+   </div>
+   
+   <div id='Content'>
+
+   <div id="loveIn">
+       <a href="#" class="lovein"><svg class='line' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><g transform='translate(2.550170, 3.550158)'><path d='M0.371729633,8.89614246 C-0.701270367,5.54614246 0.553729633,1.38114246 4.07072963,0.249142462 C5.92072963,-0.347857538 8.20372963,0.150142462 9.50072963,1.93914246 C10.7237296,0.0841424625 13.0727296,-0.343857538 14.9207296,0.249142462 C18.4367296,1.38114246 19.6987296,5.54614246 18.6267296,8.89614246 C16.9567296,14.2061425 11.1297296,16.9721425 9.50072963,16.9721425 C7.87272963,16.9721425 2.09772963,14.2681425 0.371729633,8.89614246 Z'></path><path d='M13.23843,4.013842 C14.44543,4.137842 15.20043,5.094842 15.15543,6.435842'></path></g></svg></a>
+     </div>
+     <p id="ket">Sentuh LOVEnya!</p>
+
+     <div class="kumpulanstiker">
+         <!-- Stiker untuk Konten -->
+         <img src="https://feeldreams.github.io/cilukba.gif" id="fotostiker"/>
+         <img src="https://feeldreams.github.io/pandatopi.gif" id="fotostiker1"/>
+         <img src="https://feeldreams.github.io/jogetin.gif" id="fotostiker3"/>
+         <img src="https://feeldreams.github.io/peachsad2.gif" id="fotostiker4"/>
+     </div>
+     
+     <p id="utama" class="utama"></p>
+     
+     <div><blockquote id='bq' data-text=''>
+       
+       <p id="kalimat"><span style="font-size:15px">&#65;&#107;&#117; &#80;&#117;&#110;&#121;&#97; &#80;&#101;&#115;&#97;&#110;<br>&#78;&#105;&#104; &#98;&#117;&#97;&#116; &#75;&#97;&#109;u 🫣⭐ </span></p>
+<p id="kalimatb"></p>
+
+<p id="pesan1">&#75;&#108;&#105;&#107; &#52; &#101;&#110;&#118;&#101;&#108;&#111;&#112;&#101; &#105;&#110;&#105;! &#x1F48C;</p>
+       <div id="kolombaru">
+         <li id="lv1">&#x1F48C</li>
+         <li id="lv2">&#x1F48C</li>
+         <li id="lv3">&#x1F48C</li>
+         <li id="lv4">&#x1F48C</li>
+       </div>
+
+<p id="pesan4" class="sty2b"> 
+  <span style="font-size: 15px;">M&#101;nd&#105;ng m&#97;n&#97;, &#73;nd&#111;m&#105;&#101; &#97;p&#97; M&#105;&#101; S&#101;d&#97;p? &#x1F914;</span>
+</p>
+<p id="pesan5" class="sty2b">
+  M&#101;nd&#105;ng s&#97;d&#97;r d&#105;r&#105; d&#97;r&#105;p&#97;d&#97; b&#101;rh&#97;r&#97;p &#x1F92D;<br><br>
+  <span style="font-size: 18px; margin-bottom: -50px;">&#x1F92D&#x1F606&#x1F602</span>
+</p>
+<p id="pesan6" class="sty2b left">M&#97;s&#97; m&#117;d&#97; &#105;t&#117; j&#97;ng&#97;n d&#105;b&#105;k&#105;n l&#101;l&#97;h, <br>&#107;&#101;j&#97;r m&#105;m&#112;&#105;, b&#117;k&#97;n y&#97;ng b&#105;k&#105;n r&#101;s&#97;h. <br>F&#111;k&#117;s d&#117;l&#117; s&#97;m&#97; m&#97;s&#97; d&#101;p&#97;n y&#97;ng c&#101;r&#97;h, <br>d&#97;r&#105;p&#97;d&#97; s&#105;b&#117;k n&#103;&#101;j&#97;r y&#97;ng &#117;j&#117;ngn&#121;&#97; &#101;nt&#97;h. <br><br><br><span style="font-size: 17px">🧘‍♂️💆‍♀️🌟🚀🎯✨🌞❓🌫️
+</span>
+</p>     
+       <!-- Tombol Lanjut -->
+       <p id="opsL">K&#108;&#105;&#107; &#117;&#110;&#116;&#117;&#107; &#76;&#97;&#110;&#106;&#117;&#116;</p>
+     </blockquote></div>
+
+   </div>
+    `;
+
+    // Menambahkan HTML ke dalam elemen body
+    document.body.innerHTML = htmlContent;
+});
