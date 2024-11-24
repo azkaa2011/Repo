702eb85a2bf57a6b515d03055fffe87398f02fbf
azkaa2011 <azka050511@gmail.com>
2024-11-24 21:01:47 +0700

    Update and rename root.js to Root.js

---
+++ Root.js
@@ -0,0 +1,4 @@
+const style = document.createElement('style');
+style.innerHTML = `@import url('https://fonts.googleapis.com/css2?family=Shippori+Antique:wght@400;500;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');`
+document.head.appendChild(style);
